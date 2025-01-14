fe850780838d08d928f2483b81675638c40ba754
nishantrawal2023 <nishantrawal.cs19@gmail.com>
2025-01-14 21:13:28 +0530

    added new files and modified the previous ones

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import ShopContextProvider from './context/ShopContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <ShopContextProvider>
     <App />
+  </ShopContextProvider>
   </BrowserRouter>
 );
